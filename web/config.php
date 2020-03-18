44bdc567b30a6c939b2b39e69ebc84dd2026e757
995476493 <60047524+995476493@users.noreply.github.com>
2020-03-19 02:35:49 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('APIKEY', 'AIzaSyBHBc0VEoDQ8ijvT91MIftHdhqlRU856sI');//这里改成您的api key
+define('GJ_CODE', 'HK');//国家代码
+define('SITE_NAME', 'Youtube API');//网站名字
+define('TITLENAME', 'Youtube 镜像');//网站名字
+define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh9');//一个随机字符串，加解密KEY
+define('EMAIL', '995476493@qq.com');//邮箱
 ?>
